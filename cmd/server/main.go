d11626a412b3ac78fee868352502c3a457ab6843
ognev_aa <ognev_aa@magnit.ru>
2025-05-29 23:23:33 +0300

    iter9 сохранение в файл

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-	"flag"
-	"github.com/caarlos0/env/v11"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log"
 	"net/http"
-	"strings"
 	"yupi/internal/config"
 	"yupi/internal/httptransport/handlers"
 	"yupi/internal/httptransport/middlewares"
 	"yupi/internal/logger"
 	"yupi/internal/repository"
+	"yupi/internal/service/server"
 )
-
-type Config struct {
-	ServerAddr string `env:"ADDRESS"`
-}
 
 func main() {
 	if err := logger.Initialize("info"); err != nil {
 		log.Fatal("Не удалось инициировать логгер")
 	}
 	// Инициализация конфига
-	cfg := setConfig()
+	cfg := config.SetServerConfig()
 	// Инициализация хранилища
 	storage := repository.NewMemStorage()
+	fileStorage := repository.NewFileStorage(storage)
 
-	// Инициализация сервера метрик
-	metricServer := handlers.NewMetricServer(storage)
+	// Инициализация сервера метрик, отдельно разбит на хендлер с хранилищем метрик и отдельно на сохранялку в файл
+	metricHandler := handlers.NewMetricServer(storage)
+	metricFileServer := server.NewMetricsSaver(*metricHandler, *fileStorage, &cfg)
+	err := metricFileServer.Run()
+
+	if err != nil {
+		log.Fatal("Не удалось запустить обработчик файлов")
+	}
 
 	// Инициализация роутера
 	r := chi.NewRouter()
@@ -38,35 +39,16 @@ func main() {
 	// Настройка маршрутов
 
 	r.With(middleware.AllowContentType("application/json")).
-		Post("/update/", metricServer.JSONUpdateHandler)
+		Post("/update/", metricHandler.JSONUpdateHandler)
 
 	r.With(middleware.AllowContentType("application/json")).
-		Post("/value/", metricServer.JSONValueHandler)
+		Post("/value/", metricHandler.JSONValueHandler)
 
-	r.Post("/update/{type}/{name}/{value}", metricServer.UpdateHandler)
-	r.Get("/value/{type}/{name}", metricServer.ValueHandler)
-	r.Get("/", metricServer.MainHandler)
+	r.Post("/update/{type}/{name}/{value}", metricHandler.UpdateHandler)
+	r.Get("/value/{type}/{name}", metricHandler.ValueHandler)
+	r.Get("/", metricHandler.MainHandler)
 
 	// Запуск сервера
 	log.Println("Starting server on " + cfg.ServerAddr)
 	log.Fatal(http.ListenAndServe(cfg.ServerAddr, r))
-}
-
-// Выставляет значения конфиг из аргументов командной строки
-func setConfig() Config {
-	var cfg Config
-
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	a := flag.String("a", config.DefaultServerAddr, "Адрес сервера")
-	flag.Parse()
-
-	if strings.TrimSpace(cfg.ServerAddr) == "" {
-		cfg.ServerAddr = *a
-	}
-
-	return cfg
 }
